6ee17ad17ba84855505cc0bda47ad6957630cca7
albertnguyen97 <chaunm.hmc@gmail.com>
2023-08-10 16:18:27 +0700

    upgrade pomodoro

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,33 +1,162 @@
+import tkinter as tk
+
+import time
+import math
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
+ROYALBLUE = "#4169E1"
+LIGHTSEAGREEN = "#20B2AA"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+counting = True
+continue_time = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global counting
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer", bg=YELLOW)
+    check_marks.config(text="")
+    start_button.config(state=tk.NORMAL)
+    window.config(bg=YELLOW)
+    start_button.config(bg=YELLOW)
+    canvas.config(bg=YELLOW)
+    show_history_button.config(bg=YELLOW)
+    reset_button.config(bg=YELLOW)
+    stop_resume_button.config(bg=YELLOW)
+    stop_resume_button.config(text="Stop", state=tk.DISABLED)
+    counting = True
+    global reps
+    reps = 0
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    stop_resume_button.config(state=tk.NORMAL)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps % 4 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Long Break", fg=RED, bg=LIGHTSEAGREEN)
+        window.config(bg=LIGHTSEAGREEN)
+        start_button.config(bg=LIGHTSEAGREEN)
+        show_history_button.config(bg=LIGHTSEAGREEN)
+        canvas.config(bg=LIGHTSEAGREEN)
+        reset_button.config(bg=LIGHTSEAGREEN)
+        stop_resume_button.config(bg=LIGHTSEAGREEN)
+        listbox.config(bg=LIGHTSEAGREEN)
+        check_marks.config(bg=LIGHTSEAGREEN)
+    if reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Short Break", fg=PINK, bg=ROYALBLUE)
+        window.config(bg=ROYALBLUE)
+        start_button.config(bg=ROYALBLUE)
+        canvas.config(bg=ROYALBLUE)
+        show_history_button.config(bg=ROYALBLUE)
+        reset_button.config(bg=ROYALBLUE)
+        stop_resume_button.config(bg=ROYALBLUE)
+        check_marks.config(bg=ROYALBLUE)
+        listbox.config(bg=ROYALBLUE)
+
+    else:
+        count_down(work_sec)
+        title_label.config(text="Working", fg=GREEN, bg=YELLOW)
+        window.config(bg=YELLOW)
+        start_button.config(bg=YELLOW)
+        canvas.config(bg=YELLOW)
+        show_history_button.config(bg=YELLOW)
+        reset_button.config(bg=YELLOW)
+        stop_resume_button.config(bg=YELLOW)
+        check_marks.config(bg=YELLOW)
+        listbox.config(bg=YELLOW)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    global counting, continue_time
+    if counting:
+        count_min = math.floor(count / 60)
+        count_sec = count % 60
+        if count_sec < 10:
+            count_sec = f"0{count_sec}"
+
+        canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+        if count > 0:
+            start_button.config(state=tk.DISABLED)
+            if counting:
+                global timer
+                timer = window.after(1000, count_down, count - 1)
+                continue_time = count - 1
+        else:
+            start_timer()
+            mark = ""
+            work_sessions = math.floor(reps / 2)
+            for _ in range(work_sessions):
+                mark += "✔"
+                check_marks.config(text=mark)
+
+
+# ---------------------------- STOP RESUME ------------------------------- #
+def stop_resume_timer():
+    global counting
+    counting = not counting
+    if counting:
+        stop_resume_button.config(text="stop")
+        count_down(continue_time)
+    else:
+        stop_resume_button.config(text="resume")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
-
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=100, pady=100, bg=YELLOW)
 
-
-title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"), width=12, height=2)
 title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, 'bold'))
-
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0, fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"),
+                      command=start_timer, width=5, height=1)
+start_button.grid(column=0, row=2)
+
+stop_resume_button = Button(text="Stop", highlightthickness=0, fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"),
+                            command=stop_resume_timer, width=5, height=1)
+stop_resume_button.grid(column=3, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0, fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"),
+                      command=reset_timer, width=5, height=1)
+reset_button.grid(column=3, row=3)
+
+show_history_button = Button(text="History", highlightthickness=0, fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"),
+                             width=6, height=1)
+show_history_button.grid(column=1, row=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20, "bold"))
+check_marks.grid(column=0, row=4)
+
+listbox = Listbox(window, selectmode=tk.SINGLE, bg=YELLOW)
+listbox.grid(column=1, row=4)
+
+
+
 window.mainloop()
--- test/main.py
+++ test/main.py
@@ -1,10 +1,37 @@
-import tkinter
-
-window = tkinter.Tk()
-window.title("fun fun")
-window.minsize(width=500, height=300)
-my_label = tkinter.Label(text="Input", font=("Arial", 24, "bold"))
-my_label.pack(expand=True)
+import tkinter as tk
+from tkinter import Listbox
 
 
-window.mainloop()
+def add_item():
+    item = entry.get()
+    if item:
+        listbox.insert(tk.END, item)
+        entry.delete(0, tk.END)
+
+
+def remove_item():
+    selected_index = listbox.curselection()
+    if selected_index:
+        listbox.delete(selected_index)
+
+
+root = tk.Tk()
+root.title("List GUI Example")
+
+# Create a Listbox
+listbox = Listbox(root, selectmode=tk.SINGLE)
+listbox.pack(padx=10, pady=10)
+
+# Create an Entry widget for input
+entry = tk.Entry(root)
+entry.pack(padx=10, pady=5)
+
+# Create buttons to add and remove items
+add_button = tk.Button(root, text="Add Item", command=add_item)
+add_button.pack(padx=10, pady=5)
+
+remove_button = tk.Button(root, text="Remove Selected", command=remove_item)
+remove_button.pack(padx=10, pady=5)
+
+
+root.mainloop()
