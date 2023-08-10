edadc5ee420fb544e03ac60c2c49358ba5b79a55
albertnguyen97 <chaunm.hmc@gmail.com>
2023-08-10 23:08:44 +0700

    update send mail

--- sendmail/main.py
+++ sendmail/main.py
@@ -1,4 +1,10 @@
 import smtplib
 
 my_email = "chaunm.hmc@gmail.com"
-connection = smtplib.SMTP("smtp.gmail.com")
+password = "fakepassword" # in app passwords
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="chauchaubau@gmail.com", msg="hello happy birthday be gau")
+    connection.close()
